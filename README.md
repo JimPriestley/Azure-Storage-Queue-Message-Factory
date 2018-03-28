07b71be3ca01abcf6123711e5cbd9e65cf369bad
Jim Priestley <japriest@microsoft.com>
2018-03-28 16:40:25 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Introduction 
+This a sample class library for using the Azure Storage Queue service. It includes methods for sending and receiving strings and objects as queue messages.
+
+# Getting Started
+1.	This sample is built in .NET v 4.62 in Visual Studio 2017, but can be backported without change to older versions of .NET and VS that are supported by the Microsoft WindowsAzure.Storage nuget package.
+2.	This sample was built an tested with WindowsAzure.Storage nuget package version 9.10
+
+# Azure Storage Queues
+Azure Storage Queues are different from Service Bus, in that the require the messages to be idempotent.
+A message that says the new value is 7 is a valid idempotent message. A message that says add 7 to the current total, is not a valid idempotent message.
+
+Azure Service Bus delivers messages First In, First Out, and every message is only delivered once.
+
+Azure Storage Queues make a best effort at order, and if you have multiple listeners in a distrubuted architecture, it is possible that more than one listener may receive a message.
+
+Based on the lower cost of Storage Queues versus Service Bus or Event Hubs, I often recommend it in distributed systems architectures moving to Azure PaaS, as long as we can verify that the messages meet the idempotent requirement.
+
+# Contributing
+I welcome feedback and contributions on this sample.
+
+Opportunities for extending this class include:
+- Wrapping the Async methods for Storage Queues
+- Exposing the Batch Read Methods for Storage Queues
+
+# License
+
+MIT License
+
+Copyright (c) 2018 Jim Priestley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ SQMessageFactory/StorageQueueFactory.cs
@@ -0,0 +1,171 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue; // Namespace for Queue storage types
+using System.Data.Common;
+using System.Text;
+
+
+namespace SQMessageFactory
+{
+    public class StorageQueueFactory
+    {
+        // Backing variable for queue client connection
+        CloudQueueClient queueClient;
+
+        // Backing variable for queue that this instance of the factory is connected to
+        CloudQueue queue;
+
+        // Backing variable for message context, used to delete a message that has been peeked
+        CloudQueueMessage messageContext;
+        
+        // Return Name of connected queue
+        public string QueueName
+        {
+            get { return queue.Name; }
+        }
+
+        public StorageQueueFactory(string StorageConnectionString, string QueueName)
+        {
+            // Parse the connection string and return a reference to the Storage Account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(StorageConnectionString);
+
+            queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a named queue
+            queue = queueClient.GetQueueReference(QueueName);
+
+            // Create the queue if it doesn't already exist within the Storage Account
+            queue.CreateIfNotExists();
+        }
+
+        #region Send and Receive String Messages
+            // Send a String as a Message
+            public void SendString(string Message)
+            {
+                //ToDo handle throttling exception and re throw others
+                queue.AddMessage(new CloudQueueMessage(Message));
+            }
+
+            // Reads the oldest message from the queue and returns the body as a string, deleting the message from the queue
+            public string DequeueString()
+            {
+                var messageContext = queue.GetMessage();
+                queue.DeleteMessage(messageContext);
+                return messageContext.AsString;
+            }
+
+            // Reads the oldest message from the queue and returns the body as a string, 
+            // locking the message for the default lock time without deleting the message.
+            // You must call delete message on this class within the lock time period or the message will be re-queued
+            public string PeekString()
+            {
+                messageContext = queue.PeekMessage();
+                return messageContext.AsString;
+            }
+
+            public void DeleteMessage()
+            {
+                if (!(messageContext == null))
+                {
+                    queue.DeleteMessage(messageContext);
+                    messageContext = null;
+                }
+            }
+        #endregion
+
+        #region Send and Receive Object as a Byte Array
+            // Serializes any serializable object as a byte array and then sends it
+            public void SendObject(object Message)
+            {
+                // This code will throw an exception if "Message" is not serializable
+                BinaryFormatter bf = new BinaryFormatter();
+                using (var ms = new MemoryStream())
+                {
+                    bf.Serialize(ms, Message);
+                    //This will throw an exception if the serialized Message exceeds the limit
+                    queue.AddMessage(new CloudQueueMessage(ms.ToArray()));
+                }
+            }
+
+            // Reads the oldest message from the queue as a byte array, and attempts to Deserialize it to type <T>
+            // Message is deleted from the queue if deserialization is successful
+            public T DequeueObject<T>()
+            {
+                var messageContext = queue.GetMessage();
+                T obj;
+            // This code will throw an exception if type "T" is not serializable
+
+                using (var memStream = new MemoryStream())
+                {
+                    var binForm = new BinaryFormatter();
+                    memStream.Write(messageContext.AsBytes, 0, messageContext.AsBytes.Length);
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    obj = (T)binForm.Deserialize(memStream);
+                }
+                queue.DeleteMessage(messageContext);
+                return obj;
+            }
+
+            // Reads the oldest message from the queue as a byte array, and attempts to Deserialize it to type <T>
+            // Message is locked and not deleted from the queue for the default timeout period
+            // You must call DeleteMessage() on this class before the timeout expires, or the message will be re-queued.
+
+            public T PeekObject<T>()
+            {
+                messageContext = queue.PeekMessage();
+                T obj;
+                using (var memStream = new MemoryStream())
+                {
+                    var binForm = new BinaryFormatter();
+                    memStream.Write(messageContext.AsBytes, 0, messageContext.AsBytes.Length);
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    obj = (T)binForm.Deserialize(memStream);
+                }
+              
+                return obj;
+            }
+
+        // Use the same DeleteMessage() method for objects as for strings
+
+        #endregion
+
+        // Helper Method that converts the current row of a DataReader to a delimited string, 
+        // example CSV, pass a string containing a comma as the Delimiter.
+        // Usage: StorageQueueFactory.SendString(DataReaderToDelimitedString( yourdatareader, ",");
+        public string DataReaderToDelimitedString(DbDataReader Dr, string Delimiter)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int index = 0; index < Dr.FieldCount - 1; index++)
+            {
+                if (!Dr.IsDBNull(index))
+                {
+                    string value = Dr.GetValue(index).ToString();
+                    if (Dr.GetFieldType(index) == typeof(string))
+                    {
+                        //If double quotes are used in value, ensure each are replaced but 2.
+                        if (value.IndexOf("\"") >= 0)
+                            value = value.Replace("\"", "\"\"");
+
+                        //If separator are is in value, ensure it is put in double quotes.
+                        if (value.IndexOf(Delimiter) >= 0)
+                            value = "\"" + value + "\"";
+                    }
+                    sb.Append(value);
+                }
+
+                if (index < Dr.FieldCount - 1)
+                    sb.Append(Delimiter);
+            }
+
+            if (!Dr.IsDBNull(Dr.FieldCount - 1))
+                sb.Append(Dr.GetValue(Dr.FieldCount - 1).ToString().Replace(Delimiter, " "));
+
+            return sb.ToString();
+
+        }
+
+
+    }
+}
